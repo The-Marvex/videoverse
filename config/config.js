68713b25bc0a3c06693a08d45028b3bf7ecbfb01
Manas <manas@roadzen.io>
2025-01-25 00:54:22 +0530

    added base config

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	PORT: 9443,
+	VIDEO_CONFIG: {
+		MAX_SIZE_MB: 25,
+		MIN_DURATION_SECS: 5,
+		MAX_DURATION_SECS: 300,
+		UPLOAD_DIR: './uploads'
+	},
+	API_TOKEN: 'VIDEOVERSE'
+};
